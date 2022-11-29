047f7e026712d916669cd196b08eab5dbfb60e0b
Irvin Solano <irvin.solano.ny@gmail.com>
2022-11-29 13:56:50 -0500

    function prints unbalanced binary trees correctly

--- utils2.js
+++ utils2.js
@@ -1,22 +1,20 @@
 const makeATree = (arr) => {
   const node = { val: arr[0], left: null, right: null };
-  const totalLevels = Math.sqrt(arr.length + 1) - 1;
+  const totalLevels = Math.pow(arr.length + 5, 1 / 2);
   const helper = (node, lastIndex, currentLevel) => {
     currentLevel++;
-    if (currentLevel < totalLevels) {
-      if (arr[2 * lastIndex + 1] !== null) {
+    if (arr[2 * lastIndex + 1] !== undefined && arr[2 * lastIndex + 2] !== null) {
+      if (currentLevel < totalLevels) {
         node.left = { val: arr[2 * lastIndex + 1], left: null, right: null };
         helper(node.left, 2 * lastIndex + 1, currentLevel);
-      }
-      if (arr[2 * lastIndex + 2] !== null) {
-        node.right = { val: arr[2 * lastIndex + 1 + 1], left: null, right: null };
-        helper(node.right, 2 * lastIndex + 1 + 1, currentLevel);
-      }
-    }
+        node.right = { val: arr[2 * lastIndex + 2], left: null, right: null };
+        helper(node.right, 2 * lastIndex + 2, currentLevel);
+      };
+    };
   };
   helper(node, 0, 0);
   return node;
 };
 
-// export default makeATree;
-console.log(JSON.stringify(makeATree([1, 2, 2, 3, 3, null, null, 4, 4])), '', 2);
+export default makeATree;
+// console.log(JSON.stringify(makeATree([1, 2, 2, 3, 3, null, null, 4, 4])), '', 2);
