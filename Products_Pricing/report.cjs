c5498e29c5a3e25497342ed8cd183853ffa473c4
Irvin Solano <irvin.solano.ny@gmail.com>
2022-12-01 16:44:27 -0500

    addressed style issues

--- Products_Pricing/report.cjs
+++ Products_Pricing/report.cjs
@@ -1,19 +1,14 @@
 
 // Make sure we got a filename on the command line.
 
-  // Read the file and print its contents.
-  let fs = require('fs');
-  let filename = process.argv[2];
+// Read the file and print its contents.
+const fs = require('fs');
+const filename = process.argv[2];
 
-  fs.readFile(filename, 'utf8', function(err, data) {
-    if (err) {
-      throw err;
-    } 
-    console.log(data);
-    console.log(data.split('\r\n'))
-  });
-
-  
-
-
-  
+fs.readFile(filename, 'utf8', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+  console.log(data.split('\n'));
+});
